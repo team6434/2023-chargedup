e16654a815a28042bd32e82a62b9062191fa1658
JoshuaS_09 <joshua.scott27@education.nsw.gov.au>
2023-03-07 20:55:47 +1100

    Cleaned up

--- src/main/java/frc/robot/Arm.java
+++ src/main/java/frc/robot/Arm.java
@@ -4,17 +4,11 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-
 public class Arm {
-  private PWMSparkMax armSpark;
-  private CANSparkMax CANarmSpark;
+  CANSparkMax CANarmSpark;
 
   public Arm() {
-    armSpark = new PWMSparkMax(4);
-    armSpark.setInverted(false);
-
-    CANarmSpark = new CANSparkMax(10, MotorType.kBrushless);
+    CANarmSpark = new CANSparkMax(1, MotorType.kBrushless);
     CANarmSpark.setIdleMode(IdleMode.kBrake);
     CANarmSpark.setSmartCurrentLimit(10);
     CANarmSpark.getEncoder().getPosition();
@@ -23,7 +17,7 @@ public class Arm {
   }
 
   public void raiseArm(double armSpeed) {
-    armSpark.set(Math.abs(armSpeed));
+    CANarmSpark.set(Math.abs(armSpeed));
     CANarmSpark.set(0.5);
   }
 }
